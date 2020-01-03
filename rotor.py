fa9e0ac509dfa0d3e9d4cabec1393cc06ac3588c
Peter <peter@mccloudaero.com>
2020-01-02 18:50:36 -0800

    reworked inputs and code to be able to specify inputs at locations other than the tips and root

--- rotor.py
+++ rotor.py
@@ -4,7 +4,7 @@ import sys
 import math
 import numpy as np
 import scipy.interpolate
-
+import matplotlib.pyplot as plt
 # Rotor Blade Element Analysis
 # Python Script for determining Rotor Performance using Blade Element Theory
 
@@ -26,116 +26,55 @@ airfoils_path = os.path.join(script_path,'airfoils')
 # Read inputs file
 exec(open('rotor.inputs').read())
 
-# Determine airfoil method
-try:
-  airfoil_name
-except:
-  # airfoil_name is not defined, see if tip & root airfoil names exist
-  try:
-    tip_airfoil_name
-  except NameError:
-    print('Neither airfoil_name nor tip_airfoil_name in inputs file')
-    print('Exiting!')
-    sys.exit(1)
-  else:
-    # tip_airfoil_name exists, see if root_airfoil_name exists
-    try:
-      root_airfoil_name
-    except:
-      print('root_airfoil_name is not defined in the inputs file')
-      print('Exiting!')
-      sys.exit(1)
-    else:
-      airfoil_type = 'blended' 
-else:
-  airfoil_type = 'single' 
-
-# Read airfoil data
-if airfoil_type == 'single':
+# Load airfoil data
+print('Loading Airfoil Data')
+cd_splines = []
+cl_splines = []
+cm_splines = []
+spline_dims = []
+num_airfoils = len(airfoil)
+for airfoil_name in airfoil:
+  print(airfoil_name)
   airfoil_path = os.path.join(airfoils_path,airfoil_name+'.dat')
-  airfoil = np.loadtxt(airfoil_path,skiprows=1,delimiter=',')
+  airfoil_data = np.loadtxt(airfoil_path,skiprows=1,delimiter=',')
   # Create splines
-  mach_data = airfoil[:,0]
-  alpha_data = airfoil[:,1]
-  cd_data = airfoil[:,2]
-  cl_data = airfoil[:,3]
-  cm_data = airfoil[:,4]
+  mach_data = airfoil_data[:,0]
+  alpha_data = airfoil_data[:,1]
+  cd_data = airfoil_data[:,2]
+  cl_data = airfoil_data[:,3]
+  cm_data = airfoil_data[:,4]
   # Determine Mach Range
   if min(mach_data) == max(mach_data):
     # Single Mach Number present, Create 1D splines
-    airfoil_data_type = '1D'
-    cd_spline = scipy.interpolate.splrep(alpha_data,cd_data)
-    cl_spline = scipy.interpolate.splrep(alpha_data,cl_data)
-    cm_spline = scipy.interpolate.splrep(alpha_data,cm_data)
+    spline_dims.append(1) 
+    cd_splines.append(scipy.interpolate.splrep(alpha_data,cd_data))
+    cl_splines.append(scipy.interpolate.splrep(alpha_data,cl_data))
+    cm_splines.append(scipy.interpolate.splrep(alpha_data,cm_data))
   else:
     # Multiple Mach Numbers present, Create 2D splines
-    airfoil_data_type = '2D'
-    cd_spline = scipy.interpolate.bisplrep(mach_data,alpha_data,cd_data)
-    cl_spline = scipy.interpolate.bisplrep(mach_data,alpha_data,cl_data)
-    cm_spline = scipy.interpolate.bisplrep(mach_data,alpha_data,cm_data)
-else:
-  # Tip
-  airfoil_path = os.path.join(airfoils_path,tip_airfoil_name+'.dat')
-  airfoil = np.loadtxt(airfoil_path,skiprows=1,delimiter=',')
-  # Create splines
-  mach_data = airfoil[:,0]
-  alpha_data = airfoil[:,1]
-  cd_data = airfoil[:,2]
-  cl_data = airfoil[:,3]
-  cm_data = airfoil[:,4]
-  # Determine Mach Range
-  if min(mach_data) == max(mach_data):
-    # Single Mach Number present, Create 1D splines
-    tip_airfoil_data_type = '1D'
-    tip_cd_spline = scipy.interpolate.splrep(alpha_data,cd_data)
-    tip_cl_spline = scipy.interpolate.splrep(alpha_data,cl_data)
-    tip_cm_spline = scipy.interpolate.splrep(alpha_data,cm_data)
-  else:
-    # Multiple Mach Numbers present, Create 2D splines
-    tip_airfoil_data_type = '2D'
-    tip_cd_spline = scipy.interpolate.bisplrep(mach_data,alpha_data,cd_data)
-    tip_cl_spline = scipy.interpolate.bisplrep(mach_data,alpha_data,cl_data)
-    tip_cm_spline = scipy.interpolate.bisplrep(mach_data,alpha_data,cm_data)
+    spline_dims.append(2) 
+    cd_splines.append(scipy.interpolate.bisplrep(mach_data,alpha_data,cd_data))
+    cl_splines.append(scipy.interpolate.bisplrep(mach_data,alpha_data,cl_data))
+    cm_splines.append(scipy.interpolate.bisplrep(mach_data,alpha_data,cm_data))
 
-  # Root 
-  airfoil_path = os.path.join(airfoils_path,root_airfoil_name+'.dat')
-  airfoil = np.loadtxt(airfoil_path,skiprows=1,delimiter=',')
-  # Create splines
-  mach_data = airfoil[:,0]
-  alpha_data = airfoil[:,1]
-  cd_data = airfoil[:,2]
-  cl_data = airfoil[:,3]
-  cm_data = airfoil[:,4]
-  # Determine Mach Range
-  if min(mach_data) == max(mach_data):
-    # Single Mach Number present, Create 1D splines
-    root_airfoil_data_type = '1D'
-    root_cd_spline = scipy.interpolate.splrep(alpha_data,cd_data)
-    root_cl_spline = scipy.interpolate.splrep(alpha_data,cl_data)
-    root_cm_spline = scipy.interpolate.splrep(alpha_data,cm_data)
-  else:
-    # Multiple Mach Numbers present, Create 2D splines
-    root_airfoil_data_type = '2D'
-    root_cd_spline = scipy.interpolate.bisplrep(mach_data,alpha_data,cd_data)
-    root_cl_spline = scipy.interpolate.bisplrep(mach_data,alpha_data,cl_data)
-    root_cm_spline = scipy.interpolate.bisplrep(mach_data,alpha_data,cm_data)
 
 # Initial Calculations
-tip_radius = diameter/2
-area = pi*diameter*diameter/4
-avg_chord = (root_chord + tip_chord)/2
+tip_radius = radii[-1]
+diameter = tip_radius*2
+area = pi*tip_radius**2
+avg_chord = (chord[0] + chord[-1])/2
 sigma=2*avg_chord/2/pi/tip_radius			# solidity
 
 # Blade Element Calculation
-r_inc = (tip_radius-root_radius)/(num_elements-1)
-theta_inc = (tip_theta-root_theta)/(num_elements-1)
-chord_inc = (tip_chord-root_chord)/(num_elements-1)
+#r_inc = (radii[-1]-radii[0])/(num_elements-1)
+#theta_inc = (tip_theta-root_theta)/(num_elements-1)
+#chord_inc = (tip_chord-root_chord)/(num_elements-1)
 
 
 # Initialize total_output file
 total_output = open('./output.txt','w')
 # Print Data Header
-total_output.write(' RPM,  Pitch, T(N),   P(kW), M(N-m), M(kg-cm), T(lbf), P(Hp), M(ft-lb), FM,    M_tip, v_induced\n')
+total_output.write(' RPM,  Pitch, T(N),   P(kW), M(N-m), M(kg-cm), T(lbf), P(Hp), M(ft-lb), FM,    M_tip, v_ind, cd\n')
   
 # Initialize total_output file
 radial_output = open('./radial_output.txt','w')
@@ -151,6 +90,18 @@ radial_output.write('\n')
 # theta_rad[i] = (theta[i]/180*pi)
 #print theta
 
+# Setup Rotor model
+f_chord = scipy.interpolate.interp1d(radii, chord)
+f_theta = scipy.interpolate.interp1d(radii, theta)
+
+radius_interp = np.linspace(radii[0], radii[-1], num=num_elements, endpoint=True)
+chord_interp = f_chord(radius_interp)
+theta_interp = f_theta(radius_interp)
+theta_rad = theta_interp/180.0*pi
+
+#plt.plot(radii, chord, 'o', radius_interp, chord_interp)
+#plt.show()
+
 for pitch_angle in pitch_angles:
   for RPM in RPMs:
 
@@ -159,95 +110,112 @@ for pitch_angle in pitch_angles:
     omega = n*2*pi           # angular velocity
     V_tip = omega*tip_radius # tip velocity
 
-    # Populate radii and theta lists
-    # Theta changes based on pitch
     # Initialize lists
-    radius = []
-    chord = []
-    theta = []
-    theta_rad = []
     alpha_rad = []
     for i in range(num_elements):
-      radius.append(root_radius+i*r_inc)
-      chord.append(root_chord+chord_inc)
-      theta.append(root_theta+i*theta_inc+pitch_angle)
-      theta_rad.append(theta[i]/180*pi)
       alpha_rad.append(0)
 
     # Initialize/Clear Lists
     thrust = []
     drag = []
+    drag_coef = []
     moment = []
     torque = []
 
+    inboard_airfoil_index = 0
+    outboard_airfoil_index = 1 
+
+    r_inc = radius_interp[1]-radius_interp[0] 
     for i in range(num_elements):
       # Guess at initial values of inflow and swirl factor
       # Note: swirl currently isn't used
       v_inflow = 10
       #v_swirl = 0.1
-    
+      current_r = radius_interp[i]
+      # Check inboard/outboard airfoils
+      if(current_r > radii[outboard_airfoil_index]):
+        # Advance to next set
+        inboard_airfoil_index += 1 
+        outboard_airfoil_index += 1 
+
       # Iterate at Each Blade Element
       iter = 0
       finished = False
       while( finished == False):
         v_axial = v_inflow				# axial velocity
-        v_radial = omega*radius[i]			# disk plane velocity
+        v_radial = omega*radius_interp[i]			# disk plane velocity
         #v_radial = omega*radius[i] - v_swirl	# disk plane velocity
         v_mag = pow((v_axial*v_axial+v_radial*v_radial),0.5)	# local velocity at blade (Resultant of Induced & Radial)
         mach = v_mag/a
         phi_rad = math.atan2(v_axial,v_radial)	# flow angle (radians)
         alpha_rad[i] = theta_rad[i]- phi_rad		# blade angle of attack
-        #cl,cd = airfoil_coef(alpha_rad[i])
         alpha = alpha_rad[i]*180/pi
-        alpha = min(alpha,12)
+        #alpha = min(alpha,12)
 
         # Find section coefficients
-        if airfoil_type == 'single':
-          if airfoil_data_type == '1D':
-            cd = scipy.interpolate.splev(alpha,cd_spline)
-            cl = scipy.interpolate.splev(alpha,cl_spline)
-            cm = scipy.interpolate.splev(alpha,cm_spline) 
-          elif airfoil_data_type == '2D':
-            cd = scipy.interpolate.bisplev(mach,alpha,cd_spline)
-            cl = scipy.interpolate.bisplev(mach,alpha,cl_spline)
-            cm = scipy.interpolate.bisplev(mach,alpha,cm_spline) 
+        if i == 0 or num_airfoils == 1:
+          # Root or Constant
+          dim = spline_dims[0]
+          if dim == 1:
+            cd = scipy.interpolate.splev(alpha,cd_splines[0])
+            cl = scipy.interpolate.splev(alpha,cl_splines[0])
+            cm = scipy.interpolate.splev(alpha,cm_splines[0]) 
+          elif dim == 2:
+            cd = scipy.interpolate.bisplev(mach,alpha,cd_splines[0])
+            cl = scipy.interpolate.bisplev(mach,alpha,cl_splines[0])
+            cm = scipy.interpolate.bisplev(mach,alpha,cm_splines[0]) 
+        elif i == num_elements:
+          # Tip 
+          dim = spline_dims[-1]
+          if dim == 1:
+            cd = scipy.interpolate.splev(alpha,cd_splines[-1])
+            cl = scipy.interpolate.splev(alpha,cl_splines[-1])
+            cm = scipy.interpolate.splev(alpha,cm_splines[-1]) 
+          elif dim == 2:
+            cd = scipy.interpolate.bisplev(mach,alpha,cd_splines[-1])
+            cl = scipy.interpolate.bisplev(mach,alpha,cl_splines[-1])
+            cm = scipy.interpolate.bisplev(mach,alpha,cm_splines[-1]) 
         else:
-          # Tip
-          if tip_airfoil_data_type == '1D':
-            tip_cd = scipy.interpolate.splev(alpha,tip_cd_spline) 
-            tip_cl = scipy.interpolate.splev(alpha,tip_cl_spline) 
-            tip_cm = scipy.interpolate.splev(alpha,tip_cm_spline) 
-          elif tip_airfoil_data_type == '2D':
-            tip_cd = scipy.interpolate.bisplev(mach,alpha,tip_cd_spline) 
-            tip_cl = scipy.interpolate.bisplev(mach,alpha,tip_cl_spline) 
-            tip_cm = scipy.interpolate.bisplev(mach,alpha,tip_cm_spline) 
-          # Root
-          if root_airfoil_data_type == '1D':
-            root_cd = scipy.interpolate.splev(alpha,root_cd_spline) 
-            root_cl = scipy.interpolate.splev(alpha,root_cl_spline) 
-            root_cm = scipy.interpolate.splev(alpha,root_cm_spline) 
-          elif root_airfoil_data_type == '2D':
-            root_cd = scipy.interpolate.bisplev(mach,alpha,root_cd_spline) 
-            root_cl = scipy.interpolate.bisplev(mach,alpha,root_cl_spline) 
-            root_cm = scipy.interpolate.bisplev(mach,alpha,root_cm_spline) 
+          # Interpolate between two airfoils
+          # Find inboard and outboard airfoils
+          inboard_dim = spline_dims[inboard_airfoil_index]
+          if inboard_dim == 1:
+            inboard_cd = scipy.interpolate.splev(alpha,cd_splines[inboard_airfoil_index])
+            inboard_cl = scipy.interpolate.splev(alpha,cl_splines[inboard_airfoil_index])
+            inboard_cm = scipy.interpolate.splev(alpha,cm_splines[inboard_airfoil_index]) 
+          elif inboard_dim == 2:
+            inboard_cd = scipy.interpolate.bisplev(mach,alpha,cd_splines[inboard_airfoil_index])
+            inboard_cl = scipy.interpolate.bisplev(mach,alpha,cl_splines[inboard_airfoil_index])
+            inboard_cm = scipy.interpolate.bisplev(mach,alpha,cm_splines[inboard_airfoil_index])
+          outboard_dim = spline_dims[outboard_airfoil_index]
+          if outboard_dim == 1:
+            outboard_cd = scipy.interpolate.splev(alpha,cd_splines[outboard_airfoil_index])
+            outboard_cl = scipy.interpolate.splev(alpha,cl_splines[outboard_airfoil_index])
+            outboard_cm = scipy.interpolate.splev(alpha,cm_splines[outboard_airfoil_index]) 
+          elif inboard_dim == 2:
+            outboard_cd = scipy.interpolate.bisplev(mach,alpha,cd_splines[outboard_airfoil_index])
+            outboard_cl = scipy.interpolate.bisplev(mach,alpha,cl_splines[outboard_airfoil_index])
+            outboard_cm = scipy.interpolate.bisplev(mach,alpha,cm_splines[outboard_airfoil_index])
+
           # Interpolate cd, cl, cm
-          tip_portion = float(i)/(num_elements-1)
-          root_portion = float(num_elements-1-i)/(num_elements-1)
-          cd = tip_portion*tip_cd + root_portion*root_cd
-          cl = tip_portion*tip_cl + root_portion*root_cl
-          cm = tip_portion*tip_cm + root_portion*root_cm
+          delta_r = radii[outboard_airfoil_index]-radii[inboard_airfoil_index]
+          outboard_portion = (current_r-radii[inboard_airfoil_index])/delta_r 
+          inboard_portion = 1.0-outboard_portion 
+          cd = outboard_portion*outboard_cd + inboard_portion*inboard_cd
+          cl = outboard_portion*outboard_cl + inboard_portion*inboard_cl
+          cm = outboard_portion*outboard_cm + inboard_portion*inboard_cm
     
-        # Appply drag factor
+        # Apply drag factor
         cd = cd*drag_factor   
    
         q = 0.5*rho*pow(v_mag,2)                                                # local dynamic pressure
-        DtDr = q*blades*chord[i]*(cl*math.cos(phi_rad)-cd*math.sin(phi_rad))    # thrust grading
-        DdDr = q*blades*chord[i]*(cd*math.cos(phi_rad)+cl*math.sin(phi_rad))    # drag grading
-        DmDr = q*blades*chord[i]*cm                                             # moment grading
-        DqDr = q*blades*chord[i]*radius[i]*(cd*math.cos(phi_rad)+cl*math.sin(phi_rad)) # torque grading
+        DtDr = q*num_blades*chord_interp[i]*(cl*math.cos(phi_rad)-cd*math.sin(phi_rad))    # thrust grading
+        DdDr = q*num_blades*chord_interp[i]*(cd*math.cos(phi_rad)+cl*math.sin(phi_rad))    # drag grading
+        DmDr = q*num_blades*chord_interp[i]*cm                                             # moment grading
+        DqDr = q*num_blades*chord_interp[i]*radius_interp[i]*(cd*math.cos(phi_rad)+cl*math.sin(phi_rad)) # torque grading
    
         # momentum check on inflow and swirl factors
-        v_inflow_new = DtDr/(4*pi*radius[i]*rho*v_axial)
+        v_inflow_new = DtDr/(4*pi*radius_interp[i]*rho*v_axial)
         #v_swirl_new = DqDr/(4*pi*pow(radius[i],3)*rho*v_axial*omega)
            
         # increment iteration count
@@ -269,6 +237,7 @@ for pitch_angle in pitch_angles:
       alpha = alpha_rad[i]*180/pi 
       thrust.append(DtDr*r_inc)
       drag.append(DdDr*r_inc)
+      drag_coef.append(cd)
       moment.append(DmDr*r_inc)
       torque.append(DqDr*r_inc)
 
@@ -277,17 +246,18 @@ for pitch_angle in pitch_angles:
     # Convert radians to degrees
     radial_output.write('{:d}\t'.format(RPM))
     for i in range(num_elements):
-      theta[i] = theta[i]*180/pi
+      #theta[i] = theta[i]*180/pi
       alpha_rad[i] = alpha_rad[i]*180/pi
       radial_output.write('{:5.3f}\t'.format(alpha_rad[i]))
     radial_output.write('\n')
  
     # Totals
-    T = sum(thrust)			# total thrust (N)
-    P = sum(torque)*omega/1000	# total power (kW)
-    M = sum(moment)			# total moment (N-m) 
-    M_kg = M*10.1971621298		# total moment (kg-cm)
- 
+    T = sum(thrust)                           # total thrust (N)
+    P = sum(torque)*omega/1000                # total power (kW)
+    M = sum(moment)                           # total moment (N-m) 
+    M_kg = M*10.1971621298                    # total moment (kg-cm)
+    drag_coef_avg = sum(drag_coef)/num_elements  # total thrust (N)
+
     # Ideals
     v_ideal = pow((T/(2*rho*pi*pow(tip_radius,2))),0.5) # Ideal Induced Velocity (m/s)
     P_ideal = T*v_ideal/1000 # Ideal Power (kW)
@@ -306,7 +276,7 @@ for pitch_angle in pitch_angles:
  
     FM = P_ideal/P
     M_tip = v_mag/a
-    total_output.write('{:5d}, {: 3.2f}, {:5.2f}, {:5.2f}, {: 6.3f}, {: 8.2f}, {:6.2f}, {:5.2f}, {:8.3f}, {:5.3f}, {:5.3f}, {:5.3f}\n'.format(RPM,pitch_angle,T,P,M,M_kg,T_imp,P_imp,M_imp,FM,M_tip,v_ideal))
+    total_output.write('{:5d}, {: 3.2f}, {:5.2f}, {:5.2f}, {: 6.3f}, {: 8.2f}, {:6.2f}, {:5.2f}, {:8.3f}, {:5.3f}, {:5.3f}, {:5.3f}, {:5.4}\n'.format(RPM,pitch_angle,T,P,M,M_kg,T_imp,P_imp,M_imp,FM,M_tip,v_ideal,drag_coef_avg))
 
 # Close total_output
 total_output.close()
